424904f9ee4ee868e7bb7adaf7d016a5df6f28ff
Hyeonseo Yang <35697677+KYHSGeekCode@users.noreply.github.com>
2018-09-08 20:49:37 +0900

    Create ren.bat

---
+++ ren.bat
@@ -0,0 +1,9 @@
+rem The first original version of ren
+ren *.c *.c.txt
+ren *.cpp *.cpp.txt
+ren *.java *.java.txt
+ren *.py *.py.txt
+ren *.h *.h.txt
+ren *.hpp *.hpp.txt
+ren *.asm *.asm.txt
+ren *.nas *.nas.txt
